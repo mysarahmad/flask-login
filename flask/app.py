0f0ed5f407e9b088b6f9111bd55dd27c65ca019e
mysarahmad <57366294+mysarahmad@users.noreply.github.com>
2022-05-22 21:21:05 -0700

    Add files via upload

---
+++ flask/app.py
@@ -0,0 +1,99 @@
+from tokenize import String
+from flask import Flask, render_template, url_for, flash, redirect
+from flask_login import UserMixin,LoginManager
+from wtforms_validators import Integer,ValidationError
+from flask_wtf import FlaskForm
+from flask import flash
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms.validators import DataRequired, Length, Email, EqualTo,Regexp,ValidationError
+from flask_sqlalchemy import SQLAlchemy
+
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+db=SQLAlchemy(app)
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), nullable=False, unique=True)
+    password = db.Column(db.String(80), nullable=False)
+
+   
+
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Username',
+                           validators=[DataRequired(), Length(min=2, max=20)])
+    
+    password = PasswordField('Password', validators=[DataRequired(),Regexp(
+                '^(?=\S{8,20}$)(?=.*?\d)(?=.*?[a-z])(?=.*?[A-Z])(?=.*?[^A-Za-z\s0-9])')])
+    
+    submit = SubmitField('Sign Up')
+
+    def validate_username(self,username):
+        user = User.query.filter_by(username=username.data).first()
+        if user:
+            raise ValidationError('That user name is already taken!')
+
+
+
+
+class LoginForm(FlaskForm):
+    username = StringField('Username',
+                        validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    
+    submit = SubmitField('Login')
+
+
+@app.route("/")
+@app.route("/home")
+def home():
+    return render_template('home.html')
+
+
+@app.route("/about")
+def about():
+    return render_template('about.html', title='About')
+
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        new_user = User(username=form.username.data,password=form.password.data)
+        db.session.add(new_user)
+        db.session.commit()
+        flash(f'your account has been created {form.username.data}!','sucess')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Register', form=form)
+
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user:
+            return redirect(url_for('about'))
+        else:
+            flash('Login Unsuccessful. Please check username and password', 'danger')
+    return render_template('login.html', title='Login', form=form)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
